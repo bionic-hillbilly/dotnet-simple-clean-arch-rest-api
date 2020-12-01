e56984f3daaf24864425535a2a848f01c9de2eab
Efflam POTIN <62767347+bionic-hillbilly@users.noreply.github.com>
2020-12-01 22:56:39 +0100

    feat(domain): add domain

---
+++ BionicHillbilly.BookShopService.Domain/Book.cs
@@ -0,0 +1,28 @@
+﻿namespace BionicHillbilly.BookShopService.Domain
+{
+    /// <summary>
+    /// Data representation of a book
+    /// </summary>
+    public class Book
+    {
+        /// <summary>
+        /// The identifier of the book
+        /// </summary>
+        public int BookId { get; set; }
+        
+        /// <summary>
+        /// The title of the book
+        /// </summary>
+        public string Title { get; set; }
+        
+        /// <summary>
+        /// The author of the book
+        /// </summary>
+        public string Author { get; set; }
+        
+        /// <summary>
+        /// The category of the book
+        /// </summary>
+        public string Category { get; set; }
+    }
+}
