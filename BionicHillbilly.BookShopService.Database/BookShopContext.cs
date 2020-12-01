4cb93659ac5050bd291f71e4a369eee96f50c5db
Efflam POTIN <62767347+bionic-hillbilly@users.noreply.github.com>
2020-12-01 22:57:22 +0100

    feat(db): add database context

---
+++ BionicHillbilly.BookShopService.Database/BookShopContext.cs
@@ -0,0 +1,25 @@
+﻿using BionicHillbilly.BookShopService.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace BionicHillbilly.BookShopService.Database
+{
+    /// <summary>
+    /// Defines how to access to database
+    /// </summary>
+    public class BookShopContext : DbContext
+    {
+        /// <summary>
+        /// Books table representation
+        /// </summary>
+        public DbSet<Book> Books { get; set; }
+
+        /// <summary>
+        /// BookShopContext constructor
+        /// </summary>
+        /// <param name="options">Database context options</param>
+        public BookShopContext(DbContextOptions<BookShopContext> options)
+            : base(options)
+        {
+        }
+    }
+}
